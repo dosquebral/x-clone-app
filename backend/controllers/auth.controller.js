71147c68b374de6b25d598bafe65cc7ecb483d69
DJ <imdarlenequebral@gmail.com>
2025-08-14 14:13:44 +0800

    project setup

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup = async (req, res) => {
+  res.send("You hit the signup endpoint");
+};
+
+export const login = async (req, res) => {
+  res.send("You hit the login endpoint");
+};
+
+export const logout = async (req, res) => {
+  res.send("You hit the logout endpoint");
+};

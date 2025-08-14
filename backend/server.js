1f78d3412e1dffe33deac91c5f884e681db454e6
DJ <imdarlenequebral@gmail.com>
2025-08-14 19:32:52 +0800

    database setup and auth controllers

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.route.js";
+import { connectDB } from "./config/connectDB.js";
 
 dotenv.config();
 
@@ -9,8 +11,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+  connectDB();
 });
